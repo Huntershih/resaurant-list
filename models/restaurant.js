c9f7185e995c358b0065141cf2af99640add685c
huntershih <huntershih.tw@gmail.com>
2021-09-28 00:21:29 +0800

    create mongodb seeds

---
+++ models/restaurant.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  id: {
+    type: "Number",
+    required: true
+  }, name: {
+    type: 'string',
+    required: true
+  }, name_en: {
+    type: 'string',
+    required: true
+  }, category: {
+    type: 'string',
+    required: true
+  }, image: {
+    type: 'string',
+    required: true
+  }, location: {
+    type: 'string',
+    required: true
+  }, phone: {
+    type: 'string',
+    required: true
+  }, google_map: {
+    type: 'string',
+    required: true
+  }, rating: {
+    type: 'number',
+    required: true
+  }, description: {
+    type: 'string',
+    required: true
+  }
+})
+
+module.exports = mongoose.exports('restaurants', restaurantSchema)
